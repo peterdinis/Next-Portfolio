e5b31069df5535531a6f45c1be39ebc97fde39c2
Peterdinis <peterdinis0@pobox.sk>
2022-12-03 20:27:45 +0100

    feat: Adding new pages

--- components/home/Images.tsx
+++ components/home/Images.tsx
@@ -1,7 +1,9 @@
+import Image from "next/image"
+
 export default function Images() {
     return (
-        <>
+        <div className="grid grid-flow-col grid-cols-3 grid-rows-3 gap-4 mt-2">
             IMAGES
-        </>
+        </div>
     )
 }
