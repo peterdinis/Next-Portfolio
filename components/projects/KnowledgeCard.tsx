ba839f7442cba26459b4b4878e05043fd1e5af73
Peterdinis <peterdinis0@pobox.sk>
2022-12-03 15:50:00 +0100

    feat: Working on new components wip

---
+++ components/projects/KnowledgeCard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function KnowledgeCard() {
+  return (
+    <div>KnowledgeCard</div>
+  )
+}
+
+export default KnowledgeCard
---
+++ components/projects/KnowledgeWrapper.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function KnowledgeWrapper() {
+  return (
+    <div>KnowledgeWrapper</div>
+  )
+}
+
+export default KnowledgeWrapper
