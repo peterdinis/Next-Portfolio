47ff1e9871d85cbcca80a7968028c55fc6f36850
Peterdinis <peterdinis0@pobox.sk>
2022-12-03 13:31:38 +0100

    feat: First migration

---
+++ prisma/migrations/20221203123128_initial/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Project" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "technologies" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "githubLink" TEXT NOT NULL,
+    "link" TEXT NOT NULL
+);
