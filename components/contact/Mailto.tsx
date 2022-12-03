5f2e4b53bb81358dd63b69c13a805416904f7ea0
Peterdinis <peterdinis0@pobox.sk>
2022-12-03 13:41:18 +0100

    FOO

--- components/contact/Mailto.tsx
+++ components/contact/Mailto.tsx
@@ -1,7 +1,13 @@
-export default function Mailto() {
-    return (
-        <>
-        
-        </>
-    )
+interface IProps {
+    info: string;
+}
+
+export default function Mailto({info}: IProps) {
+  return (
+    <>
+      <a className="text-3xl ml-2 text-red-900" href="pdinis1@gmail.com">
+        {info}
+      </a>
+    </>
+  );
 }
