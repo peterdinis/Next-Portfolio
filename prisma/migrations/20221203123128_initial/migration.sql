2cc4dae859229ade15fb04094a7c9f18d241ad50
Peterdinis <peterdinis0@pobox.sk>
2022-12-03 15:34:13 +0100

    FOO

--- prisma/migrations/20221203123128_initial/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Project" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "name" TEXT NOT NULL,
-    "technologies" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
-    "githubLink" TEXT NOT NULL,
-    "link" TEXT NOT NULL
-);
--- prisma/migrations/20221203123223_knowledge_model/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "Knownledge" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "name" TEXT NOT NULL,
-    "image" TEXT NOT NULL,
-    "description" TEXT NOT NULL
-);
