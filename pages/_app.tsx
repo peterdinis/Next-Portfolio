e324c5c31154fa3e122e8345d42dd0f5715b50a9
Peterdinis <peterdinis0@pobox.sk>
2022-12-03 09:55:47 +0100

    feat: Adding prisma wip

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import "../i18n/i18n"
 import Layout from '../components/shared/Layout'
 
 export default function App({ Component, pageProps }: AppProps) {
