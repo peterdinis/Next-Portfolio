82e96ea6678eb2b9acd9566fbad679f94f4211ce
Peterdinis <peterdinis0@pobox.sk>
2022-12-03 13:31:12 +0100

    feat: Adding tables

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import Hero from "../components/home/Hero";
+import Footer from "../components/shared/Footer";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Footer />
     </>
   )
 }
