e1a540aacb8fe823fbb6a64bfbcd34c9d58c944e
Peterdinis <peterdinis0@pobox.sk>
2022-12-04 12:57:09 +0100

    FOO

--- components/auth/LoginForm.tsx
+++ components/auth/LoginForm.tsx
@@ -1,54 +1,95 @@
 import React from "react";
+import Header from "../shared/Header";
 
 function LoginForm() {
   return (
     <>
-      <div className="flex items-center justify-center h-screen">
-        <div className="min-w-fit flex-col border bg-white px-6 py-14 shadow-md rounded-[4px] ">
-          <div className="mb-8 flex justify-center">
-            <img
-              className="w-24"
-              src="https://assets.leetcode.com/static_assets/public/webpack_bundles/images/logo.c36eaf5e6.svg"
-              alt=""
-            />
-          </div>
-          <div className="flex flex-col text-sm rounded-md">
+    <Header name="Registrácia" />
+     <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col">
+        <div className="mb-4">
+          <div className="mb-6">
+            <label
+              className="block text-grey-darker text-sm font-bold mb-2"
+              htmlFor="password"
+            >
+              Meno
+            </label>
             <input
-              className="mb-5 rounded-[4px] border p-3 hover:outline-none focus:outline-none hover:border-yellow-500 "
+              className="passwordInput shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3"
+              id="Meno"
               type="text"
-              placeholder="Username or Email id"
-            />
-            <input
-              className="border rounded-[4px] p-3 hover:outline-none focus:outline-none hover:border-yellow-500"
-              type="password"
-              placeholder="Password"
+              autoFocus
+              placeholder="Meno"
             />
           </div>
-          <button
-            className="mt-5 w-full border p-2 bg-gradient-to-r from-gray-800 bg-gray-500 text-white rounded-[4px] hover:bg-slate-400 scale-105 duration-300"
-            type="submit"
+          <label
+            className="block text-grey-darker text-sm font-bold mb-2"
+            htmlFor="username"
           >
-            Sign in
+            Email
+          </label>
+          <input
+            className="emailInput shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+            id="email"
+            autoFocus
+            type="email"
+            placeholder="Email"
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            className="block text-grey-darker text-sm font-bold mb-2"
+            htmlFor="username"
+          >
+            Heslo
+          </label>
+          <input
+            className="emailInput shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+            id="password"
+            placeholder="******************"
+          />
+          <button>Zobraziť heslo</button>
+        </div>
+
+        <div className="mb-6">
+          <label
+            className="block text-grey-darker text-sm font-bold mb-2"
+            htmlFor="password"
+          >
+            Potvrdenie hesla
+          </label>
+          <input
+            className="passwordInput shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3"
+            id="Potvrdenie hesla"
+            autoFocus
+          />
+          <button>Zobraziť heslo</button>
+        </div>
+
+        <div className="mb-6">
+          <label
+            className="block text-grey-darker text-sm font-bold mb-2"
+            htmlFor="password"
+          >
+            Študentské Id
+          </label>
+          <input
+            className="passwordInput shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3"
+            id="Potvrdenie hesla"
+          />
+          <button>Zobraziť heslo</button>
+        </div>
+        <div>
+          <button className="reg registerButton" type="submit">
+            Registrácia
           </button>
-          <div className="mt-5 flex justify-between text-sm text-gray-600">
-            <a href="#">Forgot password?</a>
-            <a href="#">Sign up</a>
-          </div>
-          <div className="flex justify-center mt-5 text-sm">
-            <p className="text-gray-400">or you can sign with</p>
-          </div>
-          <div className="mt-5 flex text-center text-sm text-gray-400">
-            <p>
-              This site is protected by reCAPTCHA and the Google <br />
-              <a className="underline" href="">
-                Privacy Policy
-              </a>{" "}
-              and{" "}
-              <a className="underline" href="">
-                Terms of Service
-              </a>{" "}
-              apply.
-            </p>
+          <div>
+            <a
+              className="mt-4 inline-block align-baseline font-bold text-2xl text-blue hover:text-blue-darker"
+              href="/student/login"
+            >
+              Prihlásenie tu
+            </a>
           </div>
         </div>
       </div>
