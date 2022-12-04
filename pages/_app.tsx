796372037077f187b52da850a7d550922b5143b5
Peterdinis <peterdinis0@pobox.sk>
2022-12-04 11:22:32 +0100

    chore: New packages added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,15 @@ import type { AppProps } from "next/app";
 import "../i18n/i18n";
 import Layout from "../components/shared/Layout";
 import AuthContextProvider from "../context/AuthContext";
+import { ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <AuthContextProvider>
         <Component {...pageProps} />
+        <ToastContainer />
       </AuthContextProvider>
     </Layout>
   );
