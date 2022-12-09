24a2ed47d68e8deb030002f0276b29e8e9c24925
Peterdinis <peterdinis0@pobox.sk>
2022-12-09 18:44:31 +0100

    FOO

--- cypress/component/home.cy.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-describe("Homepage tests", () => {
-  it("should navigate to the about page", () => {
-    // Start from the index page
-    cy.visit("http://localhost:3000/");
-  });
-});
