5f7fbd4a0c0c995812a086ff48b5e2bddf55f5ca
Peterdinis <peterdinis0@pobox.sk>
2022-12-10 09:18:28 +0100

    style: Moving helper style to new file

--- components/projects/KnowledgeWrapper.tsx
+++ components/projects/KnowledgeWrapper.tsx
@@ -16,18 +16,7 @@ import cssLogo from "../../images/css.png";
 import nativeLogo from "../../images/native.png";
 import jestLogo from "../../images/jest.png";
 import styles from "../../styles/Component.module.css";
-
-const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 400,
-  bgcolor: "background.paper",
-  border: "2px solid #000",
-  boxShadow: 24,
-  p: 4,
-};
+import {style} from "./styles";
 
 function KnowledgeWrapper() {
   const [open, setOpen] = React.useState(false);
