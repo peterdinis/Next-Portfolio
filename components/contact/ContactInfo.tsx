6d0621eda552981a4dfeb4631022c8489f4a5899
Peterdinis <peterdinis0@pobox.sk>
2022-12-11 12:28:40 +0100

    feat: Updating contact component

--- components/contact/ContactInfo.tsx
+++ components/contact/ContactInfo.tsx
@@ -1,20 +1,13 @@
-import { Paper } from "@mui/material";
 import Mailto from "./Mailto";
 import styles from "../../styles/Component.module.css";
 
 export default function ContactInfo() {
   return (
     <>
-      <div>
-        <div>
-          <Paper elevation={3}>
-            <h1 className={styles.infoHead}>Ak ťa moja tvorba zaujala napíš na</h1>
-            <p className={styles.infoEmail}>
-              <Mailto />
-            </p>
-          </Paper>
-        </div>
-      </div>
+      <h1 className={styles.infoHead}>Ak ťa moja tvorba zaujala napíš na</h1>
+      <p className={styles.infoEmail}>
+        <Mailto />
+      </p>
     </>
   );
 }
