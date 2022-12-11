30e2f33c1237e54501d648fae33be8b1faff04bb
Peterdinis <peterdinis0@pobox.sk>
2022-12-11 14:19:26 +0100

    feat: Updating props

--- components/projects/KnowledgeCard.tsx
+++ components/projects/KnowledgeCard.tsx
@@ -2,18 +2,14 @@ import Image, { StaticImageData } from "next/image";
 
 interface IProps {
   image: string | StaticImageData;
-    name: string
+  name: string;
 }
 
-function KnowledgeCard({image, name}: IProps) {
+function KnowledgeCard({ image, name }: IProps) {
   return (
     <div className="bg-white rounded shadow px-8 py-6 flex items-center">
       <div className="p-4 rounded">
-        <Image
-          className="w-20"
-          src={image}
-          alt="KnowledgePicture"
-        />
+        <Image className="w-20" src={image} alt="KnowledgePicture" />
       </div>
       <div className="ml-6">
         <h3 className="mb-1 leading-5 text-gray-800 font-bold text-2xl">
