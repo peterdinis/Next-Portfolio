da59973f9c9a8b007ba087431b7b167808346b72
Peterdinis <peterdinis0@pobox.sk>
2022-12-21 20:19:03 +0100

    feat: Updating portfolio

--- components/home/Images.tsx
+++ components/home/Images.tsx
@@ -1,20 +1,20 @@
 import Image from "next/image";
 import pgLogo from "../../images/pg.png";
 import queryLogo from "../../images/query.svg";
-import nativeLogo from "../../images/native.png";
 import typescriptLogo from "../../images/typescript.png";
 import jestLogo from "../../images/jest.png";
 import expressLogo from "../../images/express.png";
 import reactLogo from "../../images/react.png";
 import netLogo from "../../images/net.png";
 import nestjsLogo from "../../images/nestjs.svg";
+import nextLogo from "../../images/next-js.svg";
 
 export default function Images() {
   return (
     <div className="grid grid-flow-col grid-cols-3 grid-rows-3 gap-4 mt-2">
         <Image src={pgLogo} alt={"Postgresql"} width={300} height={300} />
         <Image src={queryLogo} alt={"React-Query"} width={300} height={300} />
-        <Image src={nativeLogo} alt={"React-Native"} width={300} height={300} />
+        <Image src={nextLogo} alt={"Nextjs"} width={300} height={300} />
         <Image src={typescriptLogo} alt={"Typescript"} width={300} height={300} />
         <Image src={jestLogo} alt={"Jest"} width={300} height={300}/>
         <Image src={expressLogo} alt={"Express"} width={300} height={300} />
--- components/projects/KnowledgeWrapper.tsx
+++ components/projects/KnowledgeWrapper.tsx
@@ -16,6 +16,7 @@ import jestLogo from "../../images/jest.png";
 import styles from "../../styles/Component.module.css";
 import {style} from "./styles";
 import netLogo from "../../images/net.png";
+import nextLogo from "../../images/next-js.svg";
 
 function KnowledgeWrapper() {
   const [open, setOpen] = React.useState(false);
@@ -75,7 +76,7 @@ function KnowledgeWrapper() {
         <KnowledgeCard image={firebase} name="Firebase" />
         <KnowledgeCard image={htmlLogo} name="Html" />
         <KnowledgeCard image={cssLogo} name="Css" />
-        <KnowledgeCard image={nativeLogo} name="React-Native" />
+        <KnowledgeCard image={nextLogo} name="Nextjs" />
         <KnowledgeCard image={jestLogo} name="Jest" />
         <KnowledgeCard image={netLogo} name="ASP.NET" />
       </div>
