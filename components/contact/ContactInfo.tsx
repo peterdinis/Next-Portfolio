dfceae4f25acdee4cbfc9e9ca43e405c38cfe322
Peterdinis <peterdinis0@pobox.sk>
2022-12-26 11:01:19 +0100

    feat: Update contactInfo

--- components/contact/ContactInfo.tsx
+++ components/contact/ContactInfo.tsx
@@ -4,7 +4,7 @@ import styles from "../../styles/Component.module.css";
 export default function ContactInfo() {
   return (
     <>
-      <h1 className={styles.infoHead}>Ak ťa moja tvorba zaujala napíš na</h1>
+      <h1 className={styles.infoHead}>Ak ťa moja tvorba zaujala napíš mi na môj email</h1>
       <p className={styles.infoEmail}>
         <Mailto />
       </p>
