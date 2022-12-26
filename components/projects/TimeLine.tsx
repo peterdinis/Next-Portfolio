6d3949bdcfcf0dd24e6ddbe11b9859ae63b28cfb
Peterdinis <peterdinis0@pobox.sk>
2022-12-26 10:58:54 +0100

    FOO

--- components/projects/TimeLine.tsx
+++ components/projects/TimeLine.tsx
@@ -6,9 +6,7 @@ function TimeLine() {
 
   return (
     <>
-      <h1 className="text-center text-4xl mt-4">
-        {t("description.work")}
-      </h1>
+      <h1 className="text-center text-4xl mt-4">{t("description.work")}</h1>
       <div className="container mx-auto w-full h-full">
         <div className="relative wrap overflow-hidden p-10 h-full">
           <div
@@ -16,9 +14,9 @@ function TimeLine() {
             style={{
               left: "50%",
             }}
-          ></div>
+          />
           <div className="mb-8 flex justify-between items-center w-full right-timeline">
-            <div className="order-1 w-5/12"></div>
+            <div className="order-1 w-5/12" />
             <div className="z-20 flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
               <h1 className="mx-auto font-semibold text-lg text-white">1</h1>
             </div>
@@ -33,7 +31,7 @@ function TimeLine() {
           </div>
 
           <div className="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
-            <div className="order-1 w-5/12"></div>
+            <div className="order-1 w-5/12" />
             <div className="z-20 flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
               <h1 className="mx-auto text-white font-semibold text-lg">2</h1>
             </div>
@@ -46,7 +44,7 @@ function TimeLine() {
           </div>
 
           <div className="mb-8 flex justify-between items-center w-full right-timeline">
-            <div className="order-1 w-5/12"></div>
+            <div className="order-1 w-5/12" />
             <div className="z-20 flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
               <h1 className="mx-auto font-semibold text-lg text-white">3</h1>
             </div>
