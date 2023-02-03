f6f4066671db66803e499408454da045dde28365
Peterdinis <peterdinis0@pobox.sk>
2023-02-03 20:26:02 +0100

    feat: Small navbar update

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Link from "next/link";
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 
 export default function Navbar() {
   const [navbar, setNavbar] = React.useState<Boolean>(false);
@@ -19,33 +21,9 @@ export default function Navbar() {
                   onClick={() => setNavbar(!navbar)}
                 >
                   {navbar ? (
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="w-6 h-6 text-white"
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
+                    <CloseIcon className="text-white" />
                   ) : (
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="w-6 h-6 text-white"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      strokeWidth={2}
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M4 6h16M4 12h16M4 18h16"
-                      />
-                    </svg>
+                    <MenuIcon className="text-white" /> 
                   )}
                 </button>
               </div>
