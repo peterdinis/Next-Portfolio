35feedf668f918af34a18eef57495f011a8d670f
Peterdinis <peterdinis0@pobox.sk>
2023-04-29 19:15:08 +0200

    feat: Update contact info

--- components/contact/ContactInfo.tsx
+++ components/contact/ContactInfo.tsx
@@ -1,10 +1,12 @@
 import Mailto from "./Mailto";
 import styles from "../../styles/Component.module.css";
+import { useTranslation } from "react-i18next";
 
 export default function ContactInfo() {
+  const { t } = useTranslation();
   return (
     <>
-      <h1 className={styles.infoHead}>Ak ťa moja tvorba zaujala napíš mi na môj email</h1>
+      <h1 className={styles.infoHead}>    {t("description.contactMe")}</h1>
       <p className={styles.infoEmail}>
         <Mailto />
       </p>
