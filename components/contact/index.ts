481ef531cef8a0574aa9bef814fe657ce7c04a87
Peterdinis <peterdinis0@pobox.sk>
2024-01-31 18:33:51 +0100

    fix: Fixing imports problems

--- components/contact/index.ts
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-
-export const ContactInfo = React.lazy(() => import('./ContactInfo'));
-export const Mailto = React.lazy(() => import('./Mailto'));
--- components/home/index.ts
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export const Hero = React.lazy(() => import('./Hero'));
-export const Info = React.lazy(() => import('./Info'));
-export const Images = React.lazy(() => import('./Images'));
